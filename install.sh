bdd18671da70021d5f9882cb421345c7feabb514
晓星! <47511745+inkstarvon@users.noreply.github.com>
2023-10-18 10:08:35 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,6 +5,8 @@
 # version: v1.0.0
 # Email: hello_linux@aliyun.com
 
+start_dir=${HOME}
+
 current_dir=$(cd $(dirname $0); pwd)
 echo '提示: 确保路径不存在中文,当前路径: :' $current_dir
 PS3='提示: 请输入序号并回车以开始: '
@@ -30,15 +32,19 @@ function sogou(){
 function fcitx(){
     #解决环境依赖性
     sudo dnf install fcitx fcitx-configtool fcitx-qt5 fcitx-sunpinyin fcitx-libs sunpinyin  gnome-tweak-tool -y
-    #开机启动fcitx
-    sudo mkdir ~/.config/autostart
-    sudo cp $current_dir/fcitx.desktop ~/.config/autostart
+    
+	#开机启动fcitx
+    sudo mkdir $start_dir/.config/autostart
+    sudo cp $current_dir/fcitx.desktop $start_dir/.config/autostart
     #重启时生效 默认输入法修改
     sudo cp $current_dir/fcitx.sh /etc/profile.d
+
     #本地默认输入法
-    echo 'export GTK_IM_MODULE=fcitx' >> ~/.bashrc
-    echo 'export QT_IM_MODULE=fcitx' >> ~/.bashrc
-    echo 'export XMODIFIERS=@im=fcitx' >> ~/.bashrc
+    echo 'export GTK_IM_MODULE=fcitx' >> $start_dir/.bashrc
+    echo 'export QT_IM_MODULE=fcitx' >> $start_dir/.bashrc
+    echo 'export XMODIFIERS=@im=fcitx' >> $start_dir/.bashrc
+
+
     #系统默认输入法
     
     echo "请重启计算机以生效！"
@@ -55,6 +61,7 @@ do
             sogou
             ;;
     esac
+    exit 0
 done
 
 
